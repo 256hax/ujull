280a79025b36e873449d5788a25ef63b2043cd66
256hax <256hax@git>
2018-09-10 22:44:52 +0900

    fix: for new_diff_codes
    delete datetime in fixtures

--- line-markers/test/controllers/diff_codes_controller_test.rb
+++ line-markers/test/controllers/diff_codes_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class DiffCodesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @diff_code = diff_codes(:one)
     @scraped_code = scraped_codes(:one)
+    @scraped_code_two = scraped_codes(:two)
+    @diff_code = diff_codes(:one)
   end
 
   test "should get index" do
@@ -12,8 +13,8 @@ class DiffCodesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_diff_codes_url(@scraped_code.id)
-    assert_response :redirect
+    get new_diff_codes_url(@scraped_code_two.id)
+    assert_response :success
   end
 
   # test "should create diff_code" do
@@ -36,6 +37,7 @@ class DiffCodesControllerTest < ActionDispatch::IntegrationTest
 
   # test "should update diff_code" do
   #   patch diff_codes_url(@diff_code), params: { diff_code: { html: @diff_code.html, scraped_code_created_at: @diff_code.scraped_code_created_at, scraped_code_id: @diff_code.scraped_code_id, scraping_page_id: @diff_code.scraping_page_id, text: @diff_code.text } }
+  #
   #   assert_redirected_to diff_codes_url(@diff_code)
   # end
 
