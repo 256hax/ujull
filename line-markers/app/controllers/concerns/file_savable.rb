6a3bcaa930e70381e61d46a8b2377c389cd9f7a5
256hax <256hax@git>
2018-10-08 18:23:27 +0900

    fix: display again page url form

--- line-markers/app/controllers/concerns/file_savable.rb
+++ line-markers/app/controllers/concerns/file_savable.rb
@@ -1,16 +1,17 @@
 module FileSavable
   extend ActiveSupport::Concern
 
+  ### return: directory_path
   def create_directory(url)
     uri = URI.parse(url) # => https://example.com/./../list.html?category=1
 
     uri_first_char = uri.host[0, 1] # => e
     uri_host = uri.host.gsub(/\/|\.\./, '_') # for security
-    dir_path = "scraped_html/#{uri_first_char}/#{uri_host}"
-    dir_full_path = "#{Rails.public_path}/scraped_html/#{uri_first_char}/#{uri_host}" # starting dir path must public path for security
+    directory_path = "scraped_html/#{uri_first_char}/#{uri_host}"
+    directory_full_path = "#{Rails.public_path}/scraped_html/#{uri_first_char}/#{uri_host}" # starting directory path must public path for security
 
-    Dir.mkdir(dir_full_path) unless ( FileTest.exist?(dir_full_path) )
-    dir_path
+    Dir.mkdir(directory_full_path) unless ( FileTest.exist?(directory_full_path) )
+    directory_path
   end
 
   def get_file_name(url)
@@ -24,10 +25,10 @@ module FileSavable
   def save_html(directory_path, file_name, request)
     # starting directory path must public path(Rails.public_path) for security
     public_and_under_directory_path = "#{Rails.public_path}/#{directory_path}"
-    dir_full_path = "#{Rails.public_path}/#{directory_path}/#{file_name}"
+    directory_full_path = "#{Rails.public_path}/#{directory_path}/#{file_name}"
 
     if FileTest.exist?(public_and_under_directory_path)
-      File.open(dir_full_path, "w") do |f|
+      File.open(directory_full_path, "w") do |f|
         request.response.body.force_encoding("utf-8") # for 2 byte chars
         f.puts( request.response.body.gsub(/\r\n|\r|\n|\t/, '') ) # get response data with deleted empty lines)
       end
