be470cb73d47839a9ba496521f5a2a915cdcbd05
256hax <256hax@git>
2018-12-03 22:24:53 +0900

    feat: add scraping latest date

--- line-markers/app/controllers/scraping_pages/htmls_controller.rb
+++ line-markers/app/controllers/scraping_pages/htmls_controller.rb
@@ -13,12 +13,17 @@ class ScrapingPages::HtmlsController < ApplicationController
     requests = scraping_pages_html(urls) # concerns/scrapable.rb
     charset = @scraping_page.charset
 
+    # Scraping loop action
     requests.map { |request|
       directory_path = @scraping_page.directory_path
       file_name = get_file_name(@scraping_page.id)
       body = request.response.body
 
       save_html(directory_path, file_name, body, charset) # concerns/file_savable.rb
+
+      # Save scraping latest date
+      @scraping_page.scraping_latest_date = DateTime.now
+      @scraping_page.save
     }
   end
 
