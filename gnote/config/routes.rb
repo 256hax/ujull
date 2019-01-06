519c80ee20d4e406943dc151441176a9e42b4232
256hax <256hax@git>
2019-01-07 01:47:43 +0900

    feat: add StaticPages Controller and static pages.
    - $ rails g controller StaticPages
    - About Page
    - HowTo Page

--- gnote/config/routes.rb
+++ gnote/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get '/articles/:action', to: 'static_pages#:action', as: :articles
+
   scope :comments do
     post '/:message_id/', to: 'comments#create', as: :comment
     get '/:message_id/new', to: 'comments#new', as: :new_comment
   end
-  #resources :comments
 
   resources :messages
 
