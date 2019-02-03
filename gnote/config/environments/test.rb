3a7e506c9e3dfc6e031ba24a69e1bbe586d88271
256hax <256hax@git>
2019-02-03 23:19:34 +0900

    feat: add Like Controller test

--- gnote/config/environments/test.rb
+++ gnote/config/environments/test.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -46,4 +46,14 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => Rails.application.credentials.gmail[:username], #gmailアドレス
+    :password => Rails.application.credentials.gmail[:password], #gmailパスワード
+    :authentication => 'login',
+  }
 end
--- gnote/spec/controllers/likes_controller_spec.rb
+++ gnote/spec/controllers/likes_controller_spec.rb
@@ -2,11 +2,52 @@ require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:valid_attributes) {{
+    :message_id => 1
+  }}
+
+  let(:invalid_attributes) {{
+    :message_id => 0 # not exist message
+  }}
+
+  let(:valid_session) { {} }
+
+  describe "POST #create" do
+    login_user
+
+    before do
+      @message = FactoryBot.create(:message)
+    end
+
+    context "with valid params" do
+      it "creates a new Like" do
+        # This code can't get response. => "expected `Like.count` to have changed by 1, but was changed by 0". Use "expect{}" code instead below.
+        # --- Error Code ---
+        # post :create, xhr: true, params: valid_attributes
+        # expect{response}.to change(Like, :count).by(1)
+        # ------------------
+        expect {
+          post :create, xhr: true, params: valid_attributes, session: valid_session }.to change(Like, :count).by(1)
+      end
+    end
+
+    context "with invalid params" do
+      it "returns error" do
+        expect {
+          post :create, xhr: true, params: invalid_attributes, session: valid_session }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "when user is not logged in" do
+      # behavior: run before(:each)
+      # source: spec/support/controller_macros.rb
+      logout_user
+
+      it "returns 401" do
+        # 401 error display a page so, It can get response.
+        post :create, xhr: true, params: valid_attributes
+        expect(response).to have_http_status(401)
+      end
     end
   end
-
 end
