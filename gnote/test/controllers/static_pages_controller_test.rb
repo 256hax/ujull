2c16f96fd1bf74eb4ca4a63682c84874431035f9
256hax <256hax@git>
2019-02-09 20:37:08 +0900

    chore: delete unused files in test directory

--- gnote/test/controllers/static_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get articles_url(:about)
-    assert_response :success
-  end
-
-end
