d172d81f72f3d049e2f02d71ce2c50538f6e68c6
256hax <256hax@git>
2019-02-10 15:35:10 +0900

    chore: add 1 comments record

--- gnote/db/seeds.rb
+++ gnote/db/seeds.rb
@@ -67,11 +67,19 @@ Comment.create!([
   },
   {
     id: 2,
+    body: 'やればできる子ｗ',
+    created_at: Date.today,
+    updated_at: Date.today,
+    message_id: 1,
+    user_id: 2
+  },
+  {
+    id: 3,
     body: 'からっぽすごいーーー！！　うちはもうすこしだ、がんばろ',
     created_at: Date.today,
     updated_at: Date.today,
     message_id: 3,
-    user_id: 2
+    user_id: 1
   }
 ])
 
